3ee22a254a1d29d45903626fd75ec9c770fcc839
altpass <altpass@yandex.ru>
2023-08-22 19:50:35 +0300

    sprint 14/ for PR

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -3,8 +3,6 @@ package ru.practicum.shareit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.Instant;
-
 @SpringBootApplication
 public class ShareItApp {
 	public static void main(String[] args) {
