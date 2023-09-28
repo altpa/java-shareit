4d5a486785c6b65bb3322610358a2f71148860de
altpass <altpass@yandex.ru>
2023-09-28 23:04:52 +0300

    sprint16 / PR

--- server/src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -16,7 +16,6 @@ import ru.practicum.shareit.exception.ObjectsDbException;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.user.repository.UserRepository;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
